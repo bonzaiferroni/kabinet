1a5a6e2f6a9da3b9209bba5aeb3457433a951a77
Luke Bollwerk <lukas8@gmail.com>
2025-05-05 09:57:18 -0600

    style purchasebar

--- library/src/commonMain/kotlin/kabinet/utils/FloatFunctions.kt
+++ library/src/commonMain/kotlin/kabinet/utils/FloatFunctions.kt
@@ -5,19 +5,18 @@ import kotlin.random.Random
 fun Float.toMetricString(): String {
     val abs = kotlin.math.abs(this)
     val (divisor, suffix) = when {
-        abs >= 1_000_000_000_000_000 -> 1_000_000_000_000_000f to "Q"
-        abs >= 1_000_000_000_000     -> 1_000_000_000_000f     to "T"
-        abs >= 1_000_000_000         -> 1_000_000_000f         to "B"
-        abs >= 1_000_000             -> 1_000_000f             to "M"
-        abs >= 1_000                 -> 1_000f                 to "k"
+        abs >= 1_000_000_000_000_000 -> 1_000_000_000_000_000f to " Q"
+        abs >= 1_000_000_000_000     -> 1_000_000_000_000f     to " T"
+        abs >= 1_000_000_000         -> 1_000_000_000f         to " B"
+        abs >= 1_000_000             -> 1_000_000f             to " M"
+        abs >= 1_000                 -> 1_000f                 to " k"
         else                         -> 1f                     to ""
     }
 
     val result = this / divisor
     val rounded = (result * 10).toInt() / 10f // 1 decimal place
 
-    val showDecimal = suffix != ""
-    return if (showDecimal) "$rounded$suffix" else "${rounded.toInt()}$suffix"
+    return if (suffix != "") "$rounded$suffix" else "${rounded.toInt()}$suffix"
 }
 
 fun Double.toMetricString() = this.toFloat().toMetricString()
