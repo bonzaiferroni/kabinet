2b6abbc273c3afab675e831c4931e946708a1556
Luke Bollwerk <lukas8@gmail.com>
2025-06-03 08:56:12 -0600

    refactor database to use UUID

--- library/src/commonMain/kotlin/kabinet/model/User.kt
+++ library/src/commonMain/kotlin/kabinet/model/User.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class User(
-    val id: Long,
+    val id: String,
     val username: String,
     val roles: RoleSet,
     val avatarUrl: String?,
