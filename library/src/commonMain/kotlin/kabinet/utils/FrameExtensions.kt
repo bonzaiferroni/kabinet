9d20fd0ca8cff22edf4da558527aa9c6d08cb676
Luke Bollwerk <lukas8@gmail.com>
2025-07-05 13:33:22 -0600

    add websocket sync

--- library/src/commonMain/kotlin/kabinet/utils/FrameExtensions.kt
+++ library/src/commonMain/kotlin/kabinet/utils/FrameExtensions.kt
@@ -12,14 +12,20 @@ inline fun <reified T> Frame.Binary.toObject() = this.readBytes()
     .let { Cbor.decodeFromByteArray<T>(it) }
 
 @OptIn(ExperimentalSerializationApi::class)
-inline fun <reified T> Frame.Binary.toObjectOrNull() = try {
-    this.readBytes().let { Cbor.decodeFromByteArray<T>(it) }
-} catch (e: Exception) {
-    println("cbor error: ${e.message}")
-    null
-}
+inline fun <reified T> Frame.Binary.toObjectOrNull() = readBytes().toObjectOrNull<T>()
 
 @OptIn(ExperimentalSerializationApi::class)
 inline fun <reified T> T.toFrame() = Frame.Binary(fin = true, data = Cbor.encodeToByteArray(this))
 
+@OptIn(ExperimentalSerializationApi::class)
+inline fun <reified T> T.toBytes() = Cbor.encodeToByteArray(this)
+
 fun ByteArray.toFrame(fin: Boolean = true) = Frame.Binary(fin = fin, data = this)
+
+@OptIn(ExperimentalSerializationApi::class)
+inline fun <reified T> ByteArray.toObjectOrNull() = try {
+    Cbor.decodeFromByteArray<T>(this)
+} catch (e: Exception) {
+    println("cbor error: ${e.message}")
+    null
+}
