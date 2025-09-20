5e5432e58993872961f6a8b89492d366e7e8bacc
Luke Bollwerk <lukas8@gmail.com>
2025-09-20 13:33:59 -0600

    sync with ponder

--- library/src/commonMain/kotlin/kabinet/model/User.kt
+++ library/src/commonMain/kotlin/kabinet/model/User.kt
@@ -1,11 +1,14 @@
 package kabinet.model
 
+import kabinet.db.TableId
+import kabinet.utils.randomUuidString
 import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
+import kotlin.jvm.JvmInline
 
 @Serializable
 data class User(
-    val id: UserId,
+    val userId: UserId,
     val username: String,
     val roles: RoleSet,
     val avatarUrl: String?,
@@ -19,4 +22,7 @@ val User.isAdmin: Boolean
 val User.isUser: Boolean
     get() = UserRole.USER in roles
 
-typealias UserId = String
+@JvmInline @Serializable
+value class UserId(override val value: String): TableId<String> {
+    companion object { fun random() = UserId(randomUuidString()) }
+}
