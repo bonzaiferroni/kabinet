6bfc91e0700466b305167758b59d5e30fc55a080
Luke Bollwerk <lukas8@gmail.com>
2025-09-21 09:49:43 -0600

    fix login issues

--- library/src/commonMain/kotlin/kabinet/api/UserApi.kt
+++ library/src/commonMain/kotlin/kabinet/api/UserApi.kt
@@ -6,10 +6,12 @@ import kabinet.model.SignUpRequest
 import kabinet.model.SignUpResult
 import kabinet.model.User
 
-object UserApi : ApiNode(null, "api/v1/user") {
+object UserApi : ApiNode(ApiNode(ApiNode(null, "api"), "v1"), "user") {
     object Login : PostEndpoint<LoginRequest, Auth>(this, "login")
     object Create : PostEndpoint<SignUpRequest, SignUpResult>(this, "create")
     object PrivateInfo : GetEndpoint<kabinet.model.PrivateInfo>(this, "private")
     object ReadInfo : GetEndpoint<User>(this)
     // object Update : PutEndpoint(this)
 }
+
+// "api/v1/user"
