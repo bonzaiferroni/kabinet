3dea17e5b3adc56730cbe2fb5035ab85ad0e1704
Luke Bollwerk <lukas8@gmail.com>
2025-10-01 09:20:40 -0600

    refactor SongNotation

--- library/src/commonMain/kotlin/kabinet/utils/ListFunctions.kt
+++ library/src/commonMain/kotlin/kabinet/utils/ListFunctions.kt
@@ -1,3 +1,5 @@
 package kabinet.utils
 
 fun <T> List<T>.replaceAt(index: Int, item: T) = mapIndexed { i, it -> if (i == index) item else it }
+
+fun <T> List<T>.removeAt(index: Int) = mapIndexedNotNull { i, it -> if (i == index) null else it }
