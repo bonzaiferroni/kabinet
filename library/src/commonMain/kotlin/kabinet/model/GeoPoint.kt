c76a7af2efef8ceda09889e88d49a4088b2dbdfa
bonzaiferroni <lukas8@gmail.com>
2026-01-22 07:49:06 -0700

    render framework

--- library/src/commonMain/kotlin/kabinet/model/GeoPoint.kt
+++ library/src/commonMain/kotlin/kabinet/model/GeoPoint.kt
@@ -1,6 +1,7 @@
 package kabinet.model
 
 import kotlinx.serialization.Serializable
+import kotlin.math.*
 
 @Serializable
 data class GeoPoint(
@@ -12,4 +13,26 @@ data class GeoPoint(
 
     fun toList(): List<Double> = listOf(lon, lat)
     fun toArray(): Array<Double> = arrayOf(lon, lat)
+
+    companion object {
+        val Denver = GeoPoint(-104.95, 39.75)
+    }
+
+    fun distanceTo(other: GeoPoint): Double {
+        val earthRadiusMeters = 6_371_000.0
+        val degToRad = PI / 180.0
+
+        val lat1 = lat * degToRad
+        val lat2 = other.lat * degToRad
+        val dLat = (other.lat - lat) * degToRad
+        val dLon = (other.lon - lon) * degToRad
+
+        val a =
+            sin(dLat / 2).pow(2) +
+                    cos(lat1) * cos(lat2) * sin(dLon / 2).pow(2)
+
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        return earthRadiusMeters * c
+    }
 }
